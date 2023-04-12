e296a5af8fb29e175bb33fceaad6ba55aa370bf8
@Harisborialas <azan6743@gmail.com>
2023-04-12 05:08:11 +0500

    Tree Link App

---
+++ components/MyFormComponent.js
@@ -0,0 +1,19 @@
+// // import zoid from 'zoid/dist/zoid.frameworks';
+// import zoid from 'zoid';
+
+// export const MyZoidComponent = zoid.create({
+//   tag: 'my-zoid-component',
+//   url: 'https://example.com/my-zoid-component',
+//   domain: 'example.com',
+//   dimensions: {
+//     width: '300px',
+//     height: '200px',
+//   },
+//   props: {
+//     message: {
+//       type: 'string',
+//       required: true,
+//     },
+//   },
+// });
+
