ee5859e1f53abb4d560ae02a7d9142b1a2ae6640
@Harisborialas <azan6743@gmail.com>
2023-04-12 13:00:44 +0500

    Tree Link App

--- pages/index.js
+++ pages/index.js
@@ -22,23 +22,25 @@ export default function Home() {
       <Head>
         <title>Link Tree App</title>
       </Head>
-      <div className={"flex flex-col items-center pt-10 justify-center"}>
-        <Image
-          className="rounded-full"
-          alt={data.name}
-          src={data.avatar}
-          width={100}
-          height={100}
-        />
-        <h1 className="name mb-2">{data.name}</h1>
-        <p className="bio mb-5">{data.bio}</p>
+      <div className=" flex flex-col items-center pt-10 justify-center">
+        <div className="maindiv">
+          <Image
+            className="rounded-full"
+            alt={data.name}
+            src={data.avatar}
+            width={100}
+            height={100}
+          />
+          <h1 className="name mb-2">{data.name}</h1>
+          <p className="bio mb-5">{data.bio}</p>
 
-        {/* {data.Links.map((linkdata) => (
+          {/* {data.Links.map((linkdata) => (
           <LinkCard image={""} key={linkdata.href} {...linkdata} />
         ))} */}
-        {data.socials.map((linkdata) => (
-          <LinkCard image={""} key={linkdata.href} {...linkdata} />
-        ))}
+          {data.socials.map((linkdata) => (
+            <LinkCard image={""} key={linkdata.href} {...linkdata} />
+          ))}
+        </div>
       </div>
     </>
   );
